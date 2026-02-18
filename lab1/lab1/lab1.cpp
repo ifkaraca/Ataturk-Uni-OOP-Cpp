d303cae8eab2a8eb06dad15b50824f65b00db906
ifkaraca <ifkaraca14@gmail.com>
2026-02-19 01:18:30 +0300

    18.02.2026 lab dersi

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	/*int sayi;
+	cout << "Bir sayi giriniz: ";
+	cin >> sayi;
+
+	cout << "Girilen sayı: " << sayi << endl;
+
+	return 0;*/
+	
+	int sayi1;
+	int sayi2;
+	int toplam;
+
+	cout << "Birinci sayiyi giriniz: ";
+	cin >> sayi1;
+
+	cout << "ikinci sayiyi giriniz: ";
+	cin >> sayi2;
+
+	toplam = sayi1 + sayi2;
+	
+	cout << "Girilen sayilarin toplami: " << toplam << endl;
+	return 0;
+}
+
